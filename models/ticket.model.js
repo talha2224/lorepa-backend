ca692a68f0abc13e0b6d85c33e0a4a7742787c9e
Talha Haider <talhahaider074@gmail.com>
2025-12-23 20:34:44 +0500

    Change senderId to use ObjectId reference

--- models/ticket.model.js
+++ models/ticket.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const messageSchema = new mongoose.Schema({
-    senderId: { type: String, required: true },
+    senderId: {  type:mongoose.Schema.Types.ObjectId, ref: "Account", required: true },
     message: { type: String, required: true },
     attachment: { type: String },
     createdAt: { type: Date, default: Date.now }
