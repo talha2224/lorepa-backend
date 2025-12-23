3db130bb84d1a53551f3920702726d58b543ff84
Talha Haider <talhahaider074@gmail.com>
2025-12-23 20:22:04 +0500

    Add route to get all tickets

--- routers/ticket.router.js
+++ routers/ticket.router.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
-const { create, getByUser, getSingle, sendMessage } = require("../services/ticket.service");
+const { create, getByUser, getSingle, sendMessage,getAll } = require("../services/ticket.service");
 const { multipleupload } = require("../config/multer.config");
 
 router.post("/create", multipleupload.single("attachment"), create);
 router.get("/user/:id", getByUser);
 router.get("/single/:id", getSingle);
 router.post("/message", multipleupload.single("attachment"), sendMessage);
+router.get("/all",getAll);
 
 module.exports = router;
